086cb476165eff42552e4fcebe8f189d6c574aa9
Basia Madej <basia@applicake.com>
2013-04-17 13:45:21 +0200

    nicer menu with kitten that can kill you

--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'sass'
+require 'compass'
+
+configure do
+  set :haml, { :format => :html5 }
+  set :scss, Compass.sass_engine_options
+  set :sass, Compass.sass_engine_options
+end
 
 get '/' do
   @id = "home"
@@ -17,4 +25,8 @@ get '/about' do
   haml :about
 end
 
-
+get '/stylesheets/*.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  filename = params[:splat].first
+  scss filename.to_sym, :views => "#{settings.root}/public/stylesheets"
+end
